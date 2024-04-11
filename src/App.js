5d501d4c01a7fa926f8c9f310d131b63b07334ac
Omelianov Nicolai <omelianov.n@gmail.com>
2024-04-11 14:15:53 +0300

    added reset btn

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from 'react';
 import './index.scss';
 
 function App() {
-  const [count, setCount] = React.useState(1);
-
+  const [count, setCount] = React.useState(0);
   const onClickPlus = () => {
-    setCount(count + 1);
+    setCount(count + 1)
   }
+
   const onClickMinus = () => {
-    setCount(count - 1);
+    setCount(count - 1)
   }
+
+  const onClickReset = () => {
+    setCount(0)
+  }
+
+
 
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
+        <button onClick={onClickMinus} onKeyDown={onClickMinus} className="minus">- Минус</button>
         <button onClick={onClickPlus} className="plus">Плюс +</button>
-        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickReset} className="reset">Сброс 0</button>
       </div>
     </div>
   );
